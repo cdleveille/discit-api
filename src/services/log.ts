203f9dca3adb26b8fb9257202b7fc2ec11309f42
Chris Leveille <cdleveille@gmail.com>
2021-11-12 17:10:06 -0500

    logging hack

--- src/services/log.ts
+++ src/services/log.ts
@@ -1,35 +1,40 @@
-import winston, { createLogger, format, transports } from "winston";
+// import winston, { createLogger, format, transports } from "winston";
 
-import config from "../helpers/config";
+// import config from "../helpers/config";
 
-const { combine, timestamp, json, prettyPrint } = format;
+// const { combine, timestamp, json, prettyPrint } = format;
 
-/**
- * Simple information loggining to combined.log | error.log
- * 
- * @method level info | error
- */
-const log: winston.Logger = createLogger({
-	level: "info",
-	format: combine(
-		json(),
-		timestamp(),
-		prettyPrint()
-	),
-	defaultMeta: {
-		service: "user-service"
-	},
-	transports: [
-		new transports.File({
-			filename: "error.log",
-			level: "error"
-		}),
-		new transports.File({
-			filename: "combined.log",
-			level: "info"
-		}),
-	],
-});
+// /**
+//  * Simple information loggining to combined.log | error.log
+//  * 
+//  * @method level info | error
+//  */
+// const log: winston.Logger = createLogger({
+// 	level: "info",
+// 	format: combine(
+// 		json(),
+// 		timestamp(),
+// 		prettyPrint()
+// 	),
+// 	defaultMeta: {
+// 		service: "user-service"
+// 	},
+// 	transports: [
+// 		new transports.File({
+// 			filename: "error.log",
+// 			level: "error"
+// 		}),
+// 		new transports.File({
+// 			filename: "combined.log",
+// 			level: "info"
+// 		}),
+// 	],
+// });
+
+const log = {
+	info: (msg: any, msg2?: any) => console.log(msg, msg2),
+	error: (msg: any, msg2?: any) => console.error(msg, msg2)
+};
 
 // if (!config.IS_PROD) {
 // 	log.add(new transports.Console({
