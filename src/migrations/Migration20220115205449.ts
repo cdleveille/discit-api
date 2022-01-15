c6bcf25ae9fe43b17b90414e2c4bbfd95de1f833
Chris Leveille <cdleveille@gmail.com>
2022-01-15 17:05:25 -0500

    adding latest full migration

--- src/migrations/Migration20220115205449.ts
+++
@@ -1,9 +0,0 @@
-import { Migration } from '@mikro-orm/migrations';
-
-export class Migration20220115205449 extends Migration {
-
-  async up(): Promise<void> {
-    this.addSql('alter table "disc" add column "color" varchar(255) not null default \'#\', add column "background_color" varchar(255) not null default \'#\';');
-  }
-
-}
